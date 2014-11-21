29e80fccdc900e14e7a908a7f73e95a9c4062362
Lawrence O'Boyle <lawrence@duratran.com>
2014-11-21 13:59:12 -0500

    Initial class

---
+++ php/Person.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created: 11/12/2014
+ * By Lawrence@40visuals.com
+ *
+ * Order handles data about the total order, and computes the total cost.
+ */
+class Person{
+    private $name;
+    private $email;
+    private $pNumber;
+
+    function __construct() {
+        $this->name = "";
+        $this->email "";
+        $this->pNumber = "";
+    }
+
+}
+?>
