f727af21357ca4ad7ff000b93e81c98eb367c8e6
= <=>
2014-11-21 14:12:11 -0500

    setup the grabbing of the submitted data

--- php/formSubmit.php
+++ php/formSubmit.php
@@ -6,4 +6,24 @@
  * Time: 1:56 PM
  */
 
+    submit();
+
+    function submit(){
+        // Person
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+
+        // Sighting
+        $date = $_POST['date'];
+        $time = $_POST['time'];
+        $location = $_POST['location'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+
+        // Bird
+        $species = $_POST['species'];
+        $desc = $_POST['desc'];
+    }
+
 ?>
