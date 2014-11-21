04c9cabeeee983d856d34397a856dffa421b9373
= <=>
2014-11-21 14:01:45 -0500

    created driver and Validate

---
+++ php/Validate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielj
+ * Date: 11/21/14
+ * Time: 1:57 PM
+ */
+
+class Validate {
+    public function __construct(){
+
+    }
+} 
---
+++ php/formSubmit.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielj
+ * Date: 11/21/14
+ * Time: 1:56 PM
+ */
+
+?>
