faed9ab3810eaae8ce806ff6c5d13f039396c0e6
Lawrence O'Boyle <lawrence@duratran.com>
2014-11-21 17:42:36 -0500

    Validation methods for Person class

--- php/Validate.php
+++ php/Validate.php
@@ -7,6 +7,7 @@
  */
 
 class Validate {
+    $pattern = "^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$"
     public function __construct(){
 
     }
@@ -36,15 +37,22 @@ class Validate {
     }
 
     public function valName($name) {
-
+        if(strlen($name) < 1 && strlen($name) > 40):
+            return 7;
+        endif
     }
 
     public function valEmail($email) {
-
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)):
+            return 8;
+        endif
     }
 
-    public function valPNumber($pNumber) {
-
+    public function valPNumberr($pNumber) {
+        if(preg_match($pattern, $pNumber)):
+            return 0
+        endif
+        return 9
     }
 }
 
