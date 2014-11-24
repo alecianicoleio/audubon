e546cc604ae7d5ea5d99c5fae2736ffcc2920015
megan <maherm@mail.gvsu.edu>
2014-11-24 16:56:58 -0500

    Fixed some validation methods

--- php/Validate.php
+++ php/Validate.php
@@ -10,59 +10,63 @@ class Validate {
     private $pPattern = "^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$";
 
     public function valDateTime($date, $minute, $hour){
-
+        return true;
     }
 
     public function valLocation($location){
-
+        return true;
     }
 
     public function valCity($city){
-
+        return true;
     }
 
     public function valState($state){
-
+        return true;
     }
 
     public function valSpecies($species) {
         if(strlen($species) <= 0) {
             //If the user does not enter a bird species
-            return 5;
+            return false;
         }
 
         if(preg_match('/d/', $species)) {
             //if the string contains a number, it is not
             //a proper bird species
-            return 5;
+            return false;
         }
+        return true;
     }
 
     public function valDescription($description) {
         if(strlen($description) <= 0) {
             //If the user does not enter a required
             //description
-            return 6;
+            return false;
         }
+        return true;
     }
 
     public function valName($name) {
-        if(strlen($name) < 1 && strlen($name) > 40):
-            return 7;
-        endif;
+        if(strlen($name) < 1 && strlen($name) > 40) {
+            return false;
+        }
+        return true;
     }
 
     public function valEmail($email) {
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)):
-            return 8;
-        endif;
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        ]
+        return true;
     }
 
     public function valPNumber($pNumber) {
-        if(preg_match($this->pattern, $pNumber)):
-            return 0;
-        endif;
-        return 9;
+        if(preg_match($this->pattern, $pNumber)) {
+            return true;
+        }
+        return false;
     }
 }
 ?>
