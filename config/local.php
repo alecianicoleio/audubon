e37318d00d20b77ffb6c46e517e3e62cda70dd28
Josh Walker <josh@tappinn.com>
2014-11-25 15:50:27 -0500

    update readme for new persistance stuff

--- config/local.php
+++ config/local.php
@@ -1,10 +1,7 @@
 <?php
 
 $local = array(
-    'driver'    =>  'pdo_mysql',
-    'user'      =>  'root',
     'password'  =>  'x0193942',
-    'dbName'    =>  'audubon'
 );
 
 $dbParams = array_replace($db,$local);
