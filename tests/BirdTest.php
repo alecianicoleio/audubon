76d482edebdb10e5fa3ddcbc6b1efbe6db75c3ac
Josh Walker <josh@tappinn.com>
2014-11-25 11:28:36 -0500

    phpunit installed

---
+++ tests/BirdTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class BirdTest
+ *
+ * Enter Class Description Here
+ *
+ * @author Joshua Walker
+ * @version 11/25/14
+ */
+
+
+
+namespace tests;
+
+
+class BirdTest extends \PHPUnit_Framework_TestCase{
+
+    public function testStuff(){
+        $this->assertTrue(true);
+    }
+
+} 
