57582a38d23a43427e6697abd11b74a1915b7680
Lawrence O'Boyle <lawrence@duratran.com>
2014-12-03 18:14:40 -0500

    This commit includes updates for the autocomplete, and because Drake wanted me to go faster, I had to do a general commit...

---
+++ php/autocomplete.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * User: Lawrence O'Boyle   lawrence@40visuals.com
+ * Date: 12/03/14
+ */
+
+namespace Audubon;
+require_once '/vendor/autoload.php';
+use Audubon\Configuration\Configuration as Configuration;
+
+// prevent direct access
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND
+    strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+if(!$isAjax) {
+    $user_error = 'Access denied - not an AJAX request...';
+    trigger_error($user_error, E_USER_ERROR);
+}
+
+// get what user typed in autocomplete input
+$term = trim($_GET['term']);
+
+$a_json = array();
+$a_json_row = array();
+
+$a_json_invalid = array(array("id" => "#", "value" => $term, "label" => "Only letters and digits are permitted..."));
+$json_invalid = json_encode($a_json_invalid);
+
+// replace multiple spaces with one
+$term = preg_replace('/\s+/', ' ', $term);
+
+// SECURITY HOLE ***************************************************************
+// allow space, any unicode letter and digit, underscore and dash
+if(preg_match("/[^\040\pL\pN_-]/u", $term)) {
+    print $json_invalid;
+    exit;
+}
+// *****************************************************************************
+
+$config = new Configuration();
+$em = $config->getEntityManager();
+$birds = $em->getRepository('Audubon\Bird')->findAll();
+$a_json = array();
+
+foreach($birds as $bird){
+    array_push($a_json, $bird->getBird()->getSpecies());
+}
+
+$json = json_encode($a_json);
+print $json;
+?>
