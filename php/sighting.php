8ec23f4c566e53805c9312a14a8370cb96552539
Lawrence O'Boyle <lawrence@duratran.com>
2014-12-05 16:08:39 -0500

    Have the selectboxes ready functioning prperly?

---
+++ php/sighting.php
@@ -0,0 +1,67 @@
+<?php
+namespace Audubon;
+require_once '../vendor/autoload.php';
+use Audubon\Configuration\Configuration as Configuration;
+
+$config = new Configuration();
+$em = $config->getEntityManager();
+$birds = $em->getRepository('Audubon\Bird')->findAll();
+$sightings = $em->getRepository('Audubon\Sighting')->findAll();
+
+foreach ( $birds as $bird ) {
+    $specieOptions[] = "<option value='{$bird->getSpecies()}'>{$bird->getSpecies()}</option>";
+}
+foreach ( $sightings as $sighting ) {
+    $locationOptions[] = "<option value='{$sighting->getLocation()}'>{$sighting->getLocation()}</option>";
+}
+//Because locations isn't a primary key, there can have multiples, so it needs to be removed.
+$locationOptions = array_unique($locationOptions);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/sightings.css" rel="stylesheet" type="text/css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+});
+</script>
+
+
+    <title>Sightings</title>
+</head>
+
+<body>
+    <div class="main">
+        <form action="">
+            <div>
+                <legend class="formtitle">Sightings</legend>
+            </div>
+
+            <!-- Select boxes -->
+            <div>
+                <label class="label">Species:</label><select class="species" name="species">
+                    <option value="selected">Select species</option>
+                    <?php echo implode("\n", $specieOptions); ?>
+                </select>
+            </div>
+
+            <div>
+                <label class="label">Location:</label><select class="location" name="location">
+                    <option value="selected">Select location</option>
+                    <?php echo implode("\n", $locationOptions); ?>
+                </select>
+            </div>
+            <!-- End Sighting Info -->
+
+            <div>
+                <input type="submit" value="Submit" />
+            </div>
+        </form>
+    </div>
+    <results>
+    </results>
+</body>
+</html>
