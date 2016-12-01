3c42d8dc67fb7d279d4efd63023ca83d5173f671
Daniel Jones <djones8520@gmail.com>
2016-12-01 09:04:19 -0500

    laravel is setup with a hello world page working

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author: Daniel Jones
+ * @version: 11/30/16
+ */
+?>
+<h1>INDEX</h1>
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,95 +1,25 @@
 <?php
-/**
- * Class TestCase
- *
- * Base test case class.
- *
- * @author Joshua Walker
- * @version 11/26/14
- */
-namespace Audubon;
-use Audubon\Configuration\Configuration;
 
-
-class TestCase extends \PHPUnit_Framework_TestCase{
+abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
     /**
-     * @var Doctrine\ORM\EntityManager
+     * The base URL to use while testing the application.
+     *
+     * @var string
      */
-    protected $em;
-    private $config;
+    protected $baseUrl = 'http://localhost';
 
-    protected function setUp(){
-        //set up test database thing..
-        //create whole new database with no data.
-        $this->config = new Configuration('testing');
-        $this->em = $this->config->getEntityManager();
-        $this->configure();
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
     }
-
-    private function configure(){
-        $db = $this->config->getDB();
-        $connection = $this->em->getConnection();
-        $connection->executeQuery("DROP DATABASE IF EXISTS `{$db['testdbname']}`");
-        $connection->executeQuery("CREATE DATABASE IF NOT EXISTS `{$db['testdbname']}`");
-        $connection->executeQuery("USE {$db['testdbname']}");
-        //
-        $this->createSchema();
-    }
-
-    private function createSchema(){
-        $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($this->em);
-        $metaData = $this->em->getMetaDataFactory()->getAllMetadata();
-        $sqlToRun = $schemaTool->getUpdateSchemaSql($metaData,false);
-        if(!count($sqlToRun))
-            return false;
-
-        $schemaTool->updateSchema($metaData);
-    }
-
-    protected function tearDown(){
-
-    }
-
-    // Setup data for test cases that query the database with species
-    protected function speciesDataSetup(){
-        $bird1 = new Bird();
-        $bird1->setSpecies("turkey",$this->em);
-        $bird1->setDescription("yummy");
-
-        $bird2 = new Bird();
-        $bird2->setSpecies("penguin",$this->em);
-        $bird2->setDescription("always formal");
-
-        $bird3 = new Bird();
-        $bird3->setSpecies("chicken", $this->em);
-        $bird3->setDescription("tastes like everything");
-
-        $this->em->persist($bird1);
-        $this->em->persist($bird2);
-        $this->em->persist($bird3);
-        $this->em->flush();
-    }
-
-    // Setup data for test cases that query the database with email
-    protected function emailDataSetup(){
-        $person1 = new Person();
-        $person1->setName("pete");
-        $person1->setPNumber("919-919-9919");
-        $person1->setEmail("pete@pete.com",$this->em);
-
-        $person2 = new Person();
-        $person2->setName("shrimp");
-        $person2->setPNumber("919-919-9919");
-        $person2->setEmail("definatly@best.com",$this->em);
-
-        $person3 = new Person();
-        $person3->setName("pizza");
-        $person3->setPNumber("919-919-9919");
-        $person3->setEmail("pizza@yummy.edu",$this->em);
-
-        $this->em->persist($person1);
-        $this->em->persist($person2);
-        $this->em->persist($person3);
-        $this->em->flush();
-    }
-} 
+}
